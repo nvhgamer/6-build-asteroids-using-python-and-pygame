4949a4c63b8551a656f12ece74a6362322d5a81a
nvhgamer <nielsvanhecke@hotmail.nl>
2025-10-07 17:20:21 +0200

    Framerate and game clock setup + added player character

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pygame
 
 from constants import *
 
+from player import Player
+
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -12,7 +14,12 @@ def main():
 
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # Main game loop
     while True:
@@ -25,7 +32,12 @@ def main():
         # screen.fill((0, 0, 0))  # Fill the screen with black
         screen.fill("black")  # Fill the screen with black
 
+        player.draw(screen) # Draw the player
+
         pygame.display.flip()  # Update the full display Surface to the screen
+
+        delta_time = clock.tick(60)  # Cap the frame rate at 60 FPS
+        dt = delta_time / 1000  # Delta time in seconds
 
     pygame.quit()
 
